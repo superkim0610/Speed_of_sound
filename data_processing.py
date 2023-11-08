daa23668b3ff8e6be9ef4fbacc9c28342c4578ad
superkim0610 <81168407+superkim0610@users.noreply.github.com>
2023-11-08 22:56:45 +0900

    data_processing.py edited -> linear regression with pandas

--- data_processing.py
+++ data_processing.py
@@ -1,8 +1,8 @@
 import csv
 import pandas as pd
-import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 def txt_to_csv():
     data_dict = {'temp': [], 'speed':[]}
@@ -16,37 +16,22 @@ def txt_to_csv():
     data_df = pd.DataFrame(data_dict)
     data_df.to_csv('data_csv.csv', index=False)
 
- 
 # by pandas ---------
 
-# data = pd.read_csv('data_csv.csv')
-# x, y = data['temp'], data['speed']
+data = pd.read_csv('data_csv.csv')
+x, y = data.iloc[:, 0], data.iloc[:, 1]
+x = x.to_frame()
 
-# print(x.head())
+print(x.head());print(y.head())
 
-# x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=42)
-# # x_train = list(map(lambda x: x[1], x_train))
-# x.reshape(-1, 1)
-# print(x_train)
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=42)
+print(x_train)
 
-# by np
+model = LinearRegression()
+model.fit(x_train, y_train)
 
-x = np.zeros()
-y = np.array()
+print("가중치(계수, 기울기 파라미터 W) :", model.coef_)
+print("편향(절편 파라미터 b) :", model.intercept_)
 
-with open('data_csv.csv', 'r') as f:
-    _f = csv.reader(f)
-    for l in _f:
-        x.append(l[0])
-        y.append(l[1])
-
-print(x);print(y)
-
-# model = LinearRegression()
-# model.fit(x_train, y_train)
-
-# print("가중치(계수, 기울기 파라미터 W) :", model.coef_)
-# print("편향(절편 파라미터 b) :", model.intercept_)
-
-# print("훈련세트 점수: {:.2f}".format( model.score(x_train, y_train) ))
-# print("테스트세트 점수: {:.2f}".format( model.score(x_test, y_test) ))
+print("훈련세트 점수: {:.2f}".format( model.score(x_train, y_train) ))
+print("테스트세트 점수: {:.2f}".format( model.score(x_test, y_test) ))
